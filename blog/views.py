8e51422ec47fec5a637e20db591e0f47f27150a4
Rojan Acharya <rojan.acharya1@gmail.com>
2024-02-02 04:15:13 +0000

    Custom Template Tags

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
+
 from .models import Post
 
 def index(request):
@@ -8,3 +10,11 @@ def index(request):
     "posts": posts
   }
   return render(request, 'blog/index.html', context)
+
+
+def post_detail(request, slug):
+  post = get_object_or_404(Post, slug=slug)
+  context = {
+    "post": post
+  }
+  return render(request, "blog/post-detail.html", context)
