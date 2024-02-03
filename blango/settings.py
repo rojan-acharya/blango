e928d4884567973efa94bba12adf6f64cfa812fc
Rojan Acharya <rojan.acharya1@gmail.com>
2024-02-03 03:56:19 +0000

    Crispy Forms added

--- blango/settings.py
+++ blango/settings.py
@@ -34,7 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'blog'
+    'blog',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 
 from .models import Post
+from .forms import CommentForm
 
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
@@ -11,10 +12,25 @@ def index(request):
   }
   return render(request, 'blog/index.html', context)
 
-
 def post_detail(request, slug):
-  post = get_object_or_404(Post, slug=slug)
-  context = {
-    "post": post
-  }
-  return render(request, "blog/post-detail.html", context)
+    post = get_object_or_404(Post, slug=slug)
+    if request.user.is_active:
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.content_object = post
+                comment.creater = request.user
+                comment.save()
+                return redirect(request.path_info)
+        else:
+            comment_form = CommentForm()
+    else:
+        comment_form = None
+
+    context = {
+        "post": post,
+        "comment_form": comment_form
+    }
+
+    return render(request, "blog/post-detail.html", context)
