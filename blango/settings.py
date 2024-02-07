f1942fc4e98a39c6d9ecd40e539bb97cac5bf476
Rojan Acharya <rojan.acharya1@gmail.com>
2024-02-07 13:06:44 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -40,10 +40,12 @@ class Dev(Configuration):
 
         'blog',
         'crispy_forms',
-        'crispy_bootstrap5'
+        'crispy_bootstrap5',
+        'debug_toolbar',
     ]
 
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -131,6 +133,8 @@ class Dev(Configuration):
 
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+    INTERNAL_IPS = ["192.168.10.226"]
 
     LOGGING = {
         "version": 1,
--- blango/urls.py
+++ blango/urls.py
@@ -1,5 +1,7 @@
+import debug_toolbar
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 import blog.views
 
@@ -7,4 +9,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index), 
     path("post/<slug>/", blog.views.post_detail, name='blog-post-detail'),
+    path("ip/", blog.views.get_ip)
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
